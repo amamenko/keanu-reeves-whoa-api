02e6e5b10d9c6fdec5a989ba59acedb53eb67959
Avi Mamenko <avimamenko@gmail.com>
2023-03-07 01:34:52 -0500

    Added Babes in Toyland whoas

--- whoaArr.ts
+++ whoaArr.ts
@@ -1,1 +1,54 @@
-export const whoaArr = [];
+export const whoaArr = [
+  {
+    movie: "Babes in Toyland (The Director's Cut)",
+    year: 1986,
+    release_date: "1986-12-19",
+    director: "Clive Donner",
+    character: "Jack Fenton / Jack-be-Nimble",
+    movie_duration: "02:25:13",
+    timestamp: "00:12:58",
+    full_line: "Caref-whoa!",
+    current_whoa_in_movie: 1,
+    total_whoas_in_movie: 2,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7G8GyvfQAl85EeFptdqhZR/abb15177e511c0f2592fa27153c7ba2f/h1qDDRTq2rf8cubZZHzeUwT5Lf6.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/3kXOsgJOAEkr8UyCnbJeOb/6cafad66c133a1635de0c3614c76e47f/Babes_in_Toyland_1__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/8myLimtO9vk8AeKrKXm2v/225a9cda44875b4df07a05eb417a1728/Babes_in_Toyland_1__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/1slIhRcNxSMAS6mt96BQDq/fdb1b35bbbf003f5ba46eca28cc62f3e/Babes_in_Toyland_1__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/4YevBqwtVUB2I5ga7LCxn/aabb3cc75c40d1e4aef6370f6fd12352/Babes_in_Toyland_1__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/4xgUe2NQL1v77piAFhVXCx/c8e121f3068fb81232cfdd8a8de0ecad/Babes_in_Toyland_1_Audio.mp3",
+  },
+  {
+    movie: "Babes in Toyland",
+    year: 1986,
+    release_date: "1986-12-19",
+    director: "Clive Donner",
+    character: "Jack Fenton / Jack-be-Nimble",
+    movie_duration: "01:35:47",
+    timestamp: "00:11:10",
+    full_line: "Whoa!",
+    current_whoa_in_movie: 2,
+    total_whoas_in_movie: 2,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7G8GyvfQAl85EeFptdqhZR/abb15177e511c0f2592fa27153c7ba2f/h1qDDRTq2rf8cubZZHzeUwT5Lf6.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/13jPjLPkOnKZBqSOMbOGAd/561383c26b1df49fd996712e68232a6f/Babes_in_Toyland_2__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2cWFB9bIDBPO1rQTLN6NWa/437a24510006f31073e983b5cce38a3b/Babes_in_Toyland_2__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/4Ngky1aUslBym2Gsl8HOcB/a24dcd724148ae65818bacf7154b6a0c/Babes_in_Toyland_2__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/6aBdXPBuru1aqArTNZ8bAv/a09bef22a14daf3fc768de5bb9b809e1/Babes_in_Toyland_2__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/2ksFG4nAWA9kfCFoMlLYhG/6b7c6de84cd2ecb71c35cc7c7e3efa3f/Babes_in_Toyland_2_Audio.mp3",
+  },
+];
