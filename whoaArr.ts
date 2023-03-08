d0b6a944fc409b908b8d148d46b7fd777d6133c3
Avi Mamenko <avimamenko@gmail.com>
2023-03-08 02:09:20 -0500

    Added 2 whoas from The Night Before (1988)

--- whoaArr.ts
+++ whoaArr.ts
@@ -51,4 +51,56 @@ export const whoaArr = [
     audio:
       "https://assets.ctfassets.net/a6ek464hq2lg/2ksFG4nAWA9kfCFoMlLYhG/6b7c6de84cd2ecb71c35cc7c7e3efa3f/Babes_in_Toyland_2_Audio.mp3",
   },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:00:24",
+    full_line: "Where am I? Whoa. Home.",
+    current_whoa_in_movie: 1,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/4OYl1Q6XWOOTM8AwN3kvCn/e69b12e8d88e2d18bd79a22dd9d0437e/The_Night_Before_1__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2F8aC8PothrZxi3bY0gsav/bf7786795905504ecbf3a02416b3efaf/The_Night_Before_1__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/3X7J8O4y5qyOls5eKS4m48/18b1739ff16a921fd435fbe61a663c0b/The_Night_Before_1__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/69ScM8l4a7u8bhqh1YomWR/e926c551cf8ad03401344d5d83787f0e/The_Night_Before_1__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/1oIWDOQ0eLFwL7lECWFOwd/2e9649fd1af20d68b7e3484d9c335e14/The_Night_Before_1_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:30:52",
+    full_line: "Whoa.",
+    current_whoa_in_movie: 2,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/4hy5CKtu8g3Lkk8HR0Jk1Q/29f9f0f216ff3cbed7f672549346424e/The_Night_Before_2__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2fA0mQh8ODUrimciZF9ETd/95ef2ab4e938cc69c6f4a24f26b63c62/The_Night_Before_2__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/tp3Q1fQjOCp5AAG7DMsRU/164b58bece69a719807945c177f6a889/The_Night_Before_2__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/1CqxNjTDO736cQkbkBOgHC/e7f89e34586f247c50b44d679d0b8c5c/The_Night_Before_2__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/2j2dPn7Bv7CUHrg2asihyW/3fbc80185c8a1b22f0962a0c7d92d894/The_Night_Before_2_Audio.mp3",
+  },
 ];
