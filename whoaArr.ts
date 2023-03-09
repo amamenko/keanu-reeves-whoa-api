03c2efb90f27697f78442810300008dd7d69c32f
Avi Mamenko <avimamenko@gmail.com>
2023-03-08 23:17:50 -0500

    Added rest of whoas for The Night Before (1988)

--- whoaArr.ts
+++ whoaArr.ts
@@ -103,4 +103,238 @@ export const whoaArr = [
     audio:
       "https://assets.ctfassets.net/a6ek464hq2lg/2j2dPn7Bv7CUHrg2asihyW/3fbc80185c8a1b22f0962a0c7d92d894/The_Night_Before_2_Audio.mp3",
   },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:34:32",
+    full_line: "Whoa.",
+    current_whoa_in_movie: 3,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2CClLzmg27DcagMdKYNEnt/0c11e13bddc88b6875829c0c7adea78b/The_Night_Before_3__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2y5lcyC9PrA8X7wwZyn7un/3641ee3565381df9ca7d6bcdfe81dbb1/The_Night_Before_3__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/5e1vv2BOepU1IIsmfW9qy1/4e8269383db506f399864077d36c3265/The_Night_Before_3__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/6nzPxbUVqnQ6UkmRfOQsd1/4f0ca38140add6609e127a7c4e13defe/The_Night_Before_3__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/5TnXcA8OUrRRU7Z0m6VQmh/dcaf53af61de708243047e602c333bad/The_Night_Before_3_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:36:20",
+    full_line: "Excuse me. Are we going to talk or are we gonna rock? Whoa!",
+    current_whoa_in_movie: 4,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/3u2P7naGT4C4E4Jrsn66tB/ff12b06cc4ace427c9d6c958dac2d877/The_Night_Before_4__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/5rZzKSRlErIfuZnvD1tZub/2ad694994f4a6bce72fb3839d3f50297/The_Night_Before_4__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/7lLGr21gk7h7U5GO58C1r6/21c9ec374f92cbf7474cf44a25ed8814/The_Night_Before_4__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/k0BKWBFnPKPp2h2EbuDpS/cf424414ff4412a2a736363182095265/The_Night_Before_4__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/38CeZY8I6oQ76bvtJQZYiH/392c93b93b93df155a491390ace0169c/The_Night_Before_4_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:40:18",
+    full_line: "Whoa! OK! Whoa. Yeah. Whoa.",
+    current_whoa_in_movie: 5,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/3F0WN03KbcevBZG4s2RBtW/dd5f4d18b3cf09b472f0d883b2ce8386/The_Night_Before_5__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/32RVuZvFq3o689nG28ODsA/e035a27f053ff695591a72c3fd281199/The_Night_Before_5__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/5XuZAcXG28AtmpAxnQlUw3/aa9916c3388d66eac0d0c6ec045e76f5/The_Night_Before_5__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/1ijhJmKdlOueij4PdEY5WN/39984060940de5b474121de3b43dd511/The_Night_Before_5__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/1APGDa0a135CUHPBtCv8Gc/cfe412893437bd46daa433cba0396757/The_Night_Before_5_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:40:21",
+    full_line: "Whoa! OK! Whoa. Yeah. Whoa.",
+    current_whoa_in_movie: 6,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/6zI8l6BBigDQFpYWX3At8n/3d81107d27b5bd736c609bab4d6ff201/The_Night_Before_6__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/eiJDBM7tXFwuXCkzqKpcm/ea00b09c5ad44a736f059aea1f2f76a1/The_Night_Before_6__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/5UCAgRS6woTwEMC4G06R3s/d59abe93d8e589f8877c4b7aa24636fa/The_Night_Before_6__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2sN9qryosgLhDNFguq0Rag/615a41c751c790ff1cec0057dc733aae/The_Night_Before_6__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/1lQYog2I58jLZt5D9heKGe/4289b17a57e2522dcc948561e1f2a532/The_Night_Before_6_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:40:23",
+    full_line: "Whoa! OK! Whoa. Yeah. Whoa.",
+    current_whoa_in_movie: 7,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/xstNl4QYB8IyUDrPFtYvM/1712673232dfc99d6de3b1659b5240c6/The_Night_Before_7__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/yEijL7bKfS0BlJDCX7NdR/e1036f71c1daf9ec43fb6c2a34d07e46/The_Night_Before_7__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/7lEILgXREfpWO7gIk5uXkR/b1445fb65a3de462e964647da154f045/The_Night_Before_7__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/W9OCgf59tey5nm7NEyiRA/f7f0d6f32b6b8f1262266ca445bcebf9/The_Night_Before_7__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/wlARKikhSxjcPVGN4W0cA/c9f574a0415cf6bcf682831f491fc2cd/The_Night_Before_7_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "00:45:27",
+    full_line: "1,500 what? Whoa! Bucks-O-Rama!",
+    current_whoa_in_movie: 8,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/6ETfxKnu7bJ75bkKlWlyvm/c3c55262aaa4313992331123a2651032/The_Night_Before_8__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/n0jHcPQ9m4cmOvDQj1BXe/81a8d774150fcab6f967c09efef57c44/The_Night_Before_8__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/tY3pFHR3V32r9vLyo4zGt/0eabac9d72bddd114462146bc4be1796/The_Night_Before_8__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/XczhnVOFtI7o96YkLTc9X/bd7d7f55efbc476b720ce289600b4fae/The_Night_Before_8__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/4Fuq84BucaLJfMx9r6ea7i/cfe716aab0e432d8f826f0c1eb9479d7/The_Night_Before_8_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "01:05:36",
+    full_line: "Whoa! Whoa! Whoa! Aah!",
+    current_whoa_in_movie: 9,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/3k6M8uHQv2bHZmOLsLFLNk/f83ec8d7e49b4e755859bfc9e796a26b/The_Night_Before_9__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2th8ieYReVAQgZfCyarhfx/825376cac95b0dc99ce3b50cbd5da4bf/The_Night_Before_9__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/1krqJK1dRu3LA5DCt5Uxj0/2ca9c169e923b64b5326a2338d578770/The_Night_Before_9__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/1twSvL5vJm8MnZ1Rbi1Px9/e6764af616a6a8c3bfc49bb711d564d7/The_Night_Before_9__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/2SixOHbX6s6gc4trbvc0kA/d742577375ceb60f5f1ae66ff9f2fa81/The_Night_Before_9_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "01:05:37",
+    full_line: "Whoa! Whoa! Whoa! Aah!",
+    current_whoa_in_movie: 10,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/79VEsw1bVeHCPut56jtgy/bb395bfbe67592b0a574909e3d5028a5/The_Night_Before_10__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/t5vW5cRj86Ey65Ps8ZOxA/13fcdfed5da8c6061874126e7daa796d/The_Night_Before_10__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/3CiWYBxvMKb9lxR9Yzguv8/603161a1f142d07c4dcbce0402a31633/The_Night_Before_10__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/1iorwcagF0KJg8d6dWFR8h/bbc9b2a2fa2bf4d09e337695b7ad5653/The_Night_Before_10__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/kljKAq91kiH4O50sDUDpk/7e11087512ffc6537bee34f3c4c7558a/The_Night_Before_10_Audio.mp3",
+  },
+  {
+    movie: "The Night Before",
+    year: 1988,
+    release_date: "1988-04-15",
+    director: "Thom Eberhardt",
+    character: "Winston Connelly",
+    movie_duration: "01:30:18",
+    timestamp: "01:05:38",
+    full_line: "Whoa! Whoa! Whoa! Aah!",
+    current_whoa_in_movie: 11,
+    total_whoas_in_movie: 11,
+    poster:
+      "https://images.ctfassets.net/a6ek464hq2lg/7GlORd4lwAJs0tPtqzPmy2/815d20f7c8f452f62e8e7fbeb1f29ace/fAm7q29aXTT0eDKPxuXzAGm6dKR.jpeg",
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/2BRbwUHPRpKuDEEMHWnZFm/0938384d4736ab9c9042175bd8bfe420/The_Night_Before_11__1080p_.mp4",
+      "720p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/4lMyddkZAqAPYtlFm8Ac6A/e738d1d991f0cf8e0dcc08102c7d2c8b/The_Night_Before_11__720p_.mp4",
+      "480p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/6rikOc6s5KDNNngI7FiCK2/7d4376f76f257780b0bfb2eb700dda5d/The_Night_Before_11__480p_.mp4",
+      "360p":
+        "https://videos.ctfassets.net/a6ek464hq2lg/XNdPiVuSxDjUa3xU6hPWL/77b88c926015f88fc3bef1346808a8be/The_Night_Before_11__360p_.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/a6ek464hq2lg/4y9cXxSM5xfQeMF840E4FQ/d000b905499ee5d301adcb07befda667/The_Night_Before_11_Audio.mp3",
+  },
 ];
